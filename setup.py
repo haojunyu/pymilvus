0a529b0c88e4512395188a5e481d7229e6bf8000
haojunyu <haojunyu@soulapp.com>
2021-11-09 23:58:03 +0800

    update setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         "ujson>=2.0.0",
         "mmh3",
         "pandas==1.1.5; python_version<'3.7'",
-        "pandas==1.2.4; python_version>'3.6'",
+        "pandas==1.3.0; python_version>'3.6'",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
